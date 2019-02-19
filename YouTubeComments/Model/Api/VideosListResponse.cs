e6a2a6004a4ce99d09eaae36e204960a71f5b942
Grzegorz Łagocki <grzesieklagocki@gmail.com>
2019-02-19 01:50:47 +0100

    Added VideosListResponse and CommentViewModel.

---
+++ YouTubeComments/Model/Api/VideosListResponse.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeComments.Model.Api
+{
+    public class Thumbnail
+    {
+        public string url;
+        public int width;
+        public int height;
+    }
+
+    public class Thumbnails
+    {
+        public Thumbnail @default;
+        public Thumbnail medium;
+        public Thumbnail high;
+        public Thumbnail standard;
+        public Thumbnail maxres;
+    }
+
+    public class Snippet
+    {
+        public DateTime publishedAt;
+        public string channelId;
+        public string title;
+        public string description;
+        public Thumbnails thumbnails;
+        public string channelTitle;
+        public List<string> tags;
+    }
+
+    public class ContentDetails
+    {
+        public string duration;
+        public string dimension;
+        public string definition;
+        public string caption;
+    }
+
+    public class Statistics
+    {
+        public string viewCount;
+        public string likeCount;
+        public string dislikeCount;
+        public string favoriteCount;
+        public string commentCount;
+    }
+
+    public class Video
+    {
+        public string id;
+        public Snippet snippet;
+        public ContentDetails contentDetails;
+        public Statistics statistics;
+    }
+
+    public class RootObject
+    {
+        public List<Video> items;
+    }
+}
---
+++ YouTubeComments/ViewModel/CommentViewModel.cs
@@ -0,0 +1,44 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+using YouTubeComments.Model.Api;
+using System.Collections.ObjectModel;
+
+namespace YouTubeComments.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    public class CommentViewModel : ViewModelBase
+    {
+        public string ID { get; private set; }
+        public string Text { get; private set; }
+        public string AuthorChannelUrl { get; private set; }
+        public string AuthorDisplayName { get; private set; }
+        public string AuthorProfileImageUrl { get; private set; }
+        public int LikeCount { get; private set; }
+        public int ReplyCount { get; private set; }
+        public DateTime PublishedAt { get; private set; }
+
+        public bool CanLoadReplies { get => ReplyCount > 0; }
+        public bool IsReply { get => ID == null; }
+
+        public ObservableCollection<Comment> Replies { get; private set; }
+
+
+
+        public CommentViewModel(Comment comment, string id = null, int replyCount = 0)
+        {
+            ID = id;
+            Text = comment.textDisplay;
+            AuthorChannelUrl = comment.authorChannelUrl;
+            AuthorDisplayName = comment.authorDisplayName;
+            AuthorProfileImageUrl = comment.authorProfileImageUrl;
+            LikeCount = comment.likeCount;
+            ReplyCount = replyCount;
+            PublishedAt = comment.publishedAt;
+        }
+    }
+}
